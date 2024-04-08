b935dfc805d6aa1e25a9307d7356518a00214ddd
leeeemj <leemj2944@gmail.com>
2024-04-08 13:44:32 +0900

    fix : week2 수정
    uploaded_at -> updated_at 변경
    recomment 관계필드 추가
    timezone.now -> auto_now=True 변경

--- comments/models.py
+++ comments/models.py
@@ -6,5 +6,5 @@ class Comment(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments',null=True)
     post = models.ForeignKey('posts.Post',on_delete=models.CASCADE,related_name='comments',null=True)
     content=models.CharField(max_length=500,null=True,blank=True)
-    created_at=models.DateTimeField(default=timezone.now)
-    uploaded_at=models.DateTimeField(auto_now=True)
+    created_at=models.DateTimeField(auto_now=True)
+    updated_at=models.DateTimeField(auto_now=True)
