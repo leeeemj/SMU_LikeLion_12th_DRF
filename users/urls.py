415e3746482dbc3b0b6549de2a14d79b2a142b24
leeeemj <leemj2944@gmail.com>
2024-05-07 18:21:16 +0900

    views.py : users

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from users import views
+#진짜 디자인할 패턴들, url나누는 기준은 view
+urlpatterns = [
+    #path('', views.user_list_api_view,name='user-list'),#users로 시작하는 모든 패턴은 users.urls가서 찾아라
+    path('',views.user_join, name='user-join'),
+    path('<int:pk>/', views.user_detail,name='user-detail')
+]
+#전체조회는 list
+#단일조회는 retrieve
+
+#put할 때 username이랑 password 써야만 수정 가능함... 왜지 ? 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from posts.models import Post
+from users.serializers import UserSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from users.models import User
 # Create your views here.
+
+#회원가입
+@api_view(['POST'])
+def user_join(request):
+    if request.method=='POST':
+        # username=request.data.get('username')
+        # password=request.data.get('password')
+        # user=User.objects.create_user(username=username,password=password)
+        serializer=UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#회원 정보 수정, 탈퇴, 보기
+@api_view(['GET','PUT','DELETE'])
+def user_detail(request,pk):
+    try:
+        user=User.objects.get(id=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET':
+        serializer=UserSerializer(user)
+        return Response(serializer.data)
+    elif request.method=='PUT':
+        serializer=UserSerializer(user,data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_404_NOT_FOUND) 
+    elif request.method=='DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
